4ddaf61ca5eaa67fcdbb9c141cbe2fe12e066027
KTM-LEARNOVA <learnova.ananya@gmail.com>
2024-12-23 16:32:54 +0530

    Update ThemeProvider.tsx

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,8 +1,20 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import type { ThemeProviderProps as NextThemesProviderProps } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+interface ThemeProviderProps {
+  children: React.ReactNode
 }
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+    >
+      {children}
+    </NextThemesProvider>
+  )
+}
+
