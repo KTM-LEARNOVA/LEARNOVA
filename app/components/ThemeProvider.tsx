a6f6d44ff382c4c15daca045c8af37a382dbacb0
KTM-LEARNOVA <learnova.ananya@gmail.com>
2024-12-23 16:29:03 +0530

    Update ThemeProvider.tsx

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -6,4 +6,3 @@ import type { ThemeProviderProps as NextThemesProviderProps } from 'next-themes'
 export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
